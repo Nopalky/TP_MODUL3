4dc36aebb565e1f6ac9dbd329870c25c0ac607d1
opal <novalguci20@gmail.com>
2026-03-08 15:24:14 +0700

    Menambahkan tampilan GUI kalkulator

---
+++ source/repos/TP_MODUL_103022400014/TP_MODUL_103022400014/Form1.cs
@@ -0,0 +1,30 @@
+namespace TP_MODUL_103022400014
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
